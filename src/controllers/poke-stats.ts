bfce85e2554e30dad2b04596c59f9017b6dfaea9
alonso804 <alonsobs804@gmail.com>
2023-10-07 12:02:36 -0500

    feat: add execution time logs

--- src/controllers/poke-stats.ts
+++ src/controllers/poke-stats.ts
@@ -1,4 +1,5 @@
 import type { Request, Response } from 'express';
+import { performance } from 'perf_hooks';
 import { logger } from 'src/logger';
 import StatsModel from 'src/models/stat';
 import { z } from 'zod';
@@ -9,6 +10,8 @@ const getSchema = z.object({
 
 class PokeStatsController {
   static async get(req: Request, res: Response): Promise<void> {
+    const start = performance.now();
+
     const data = getSchema.safeParse(req.params);
 
     if (!data.success) {
@@ -21,15 +24,17 @@ class PokeStatsController {
     const stats = await StatsModel.findById(id);
 
     if (!stats) {
-      logger.info({ microservice: 'poke-stats', message: 'Not found' });
+      const end = performance.now();
+      logger.warn({ microservice: 'poke-stats', message: 'Not found', time: end - start });
 
       res.status(404).send({ message: 'Stat not found' });
       return;
     }
 
-    logger.info({ microservice: 'poke-stats', message: 'Read from mongodb' });
+    const end = performance.now();
+    logger.info({ microservice: 'poke-stats', message: 'Read from mongodb', time: end - start });
 
-    res.status(200).send(stats);
+    res.status(200).send({ stats });
   }
 }
 
